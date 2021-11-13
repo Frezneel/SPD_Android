7f2ba89c6f806104f4d1d408e5c31bda3ccaafcc
Galih Muhammad Ichsan <nyomangalih12@gmail.com>
2021-11-13 20:58:41 +0700

    Fix Force close Logout

--- app/src/main/java/com/spdapps/FrgMenu/Frg_akun.java
+++ app/src/main/java/com/spdapps/FrgMenu/Frg_akun.java
@@ -12,21 +12,24 @@ import androidx.fragment.app.Fragment;
 import com.example.spdsosialdanphysicaldistance.R;
 import com.spdapps.Activity.LoginMhs;
 import com.spdapps.Activity.MainActivity;
+import com.spdapps.Model.SessionManager;
 
 public class Frg_akun extends Fragment {
     View view;
     Button btLogoutAkun;
+    SessionManager sessionManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frg_akun, container, false);
         btLogoutAkun = view.findViewById(R.id.bt_logoutAkun);
+        sessionManager = new SessionManager(getActivity());
         btLogoutAkun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent logout = new Intent(getActivity(), LoginMhs.class);
-                logout.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_HISTORY);
+                sessionManager.logoutSession();
                 startActivity(logout);
                 getActivity().finish();
             }
